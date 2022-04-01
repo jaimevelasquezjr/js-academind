1551148d2ec3c2310ca37c66c29c5e850a094495
Coding Freqs <jongvlsqz@gmail.com>
2022-04-01 18:33:18 +0800

    Updates js basics

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,14 @@
+const userInput = 30;
+let result;
+
+result = 18 + userInput; // #2
+result = result - 8; // #3
+result = result * 2; // #3
+result = result / 10; // #3
+
+// Think about the value stored in the user input variable you also created - did that value change?
+// Answer: No
+
+console.log(result);
+alert(`Result: ${result}`);
+alert(`User Input: ${userInput}`);
