bfcdfcdec625a6d466e9fc90405e2f2b57bfac72
Coding Freqs <jongvlsqz@gmail.com>
2022-04-04 19:43:51 +0800

    basic 02 updates

--- basics-02-starting-project/assets/scripts/app.js
+++ basics-02-starting-project/assets/scripts/app.js
@@ -1,8 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+   const result = num1 + num2;
+   return result;
+}
 
-let calculationDescription = `( ${defaultResult} + 10)* 3 / 2 - 1`;
+let compute = add(4, 4);
+console.log(compute);
 
-outputResult(currentResult, calculationDescription);
+// currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+// let calculationDescription = `( ${defaultResult} + 10)* 3 / 2 - 1`;
+
+// outputResult(currentResult, calculationDescription);
